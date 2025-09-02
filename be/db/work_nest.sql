71f06fed67d10de5fcd5c4883b3be6b285eb7b32
Doan Giang <dvg8.vcc@gmail.com>
2025-09-02 23:28:24 +0700

    update

--- be/db/work_nest.sql
+++ be/db/work_nest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 01, 2025 at 09:12 AM
+-- Generation Time: Sep 02, 2025 at 04:27 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
